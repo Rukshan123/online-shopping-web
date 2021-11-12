53bc101a4c31f16f6fba1dcc9f9afd12c0f67f2d
Rukshan123 <rukzmadu@gmail.com>
2021-11-12 14:15:57 +0530

    get user data and genarate and return token

---
+++ backend/utils.js
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken'
+export const genarateToken = (user) => {
+  return jwt.sign(
+    {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
+    process.env.JWT_SECRET || 'somethingsecret',
+    { expiresIn: '30d' }
+  )
+}
