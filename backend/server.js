97afb28e07022124af0e877f9330f0c8170e20bc
Rukshan123 <rukzmadu@gmail.com>
2021-11-12 14:15:09 +0530

    use donenv and add two middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 import productRouter from './routers/productRouter.js'
 import userRouter from './routers/userRouter.js'
 
+//use secure the token
+dotenv.config()
 const app = express()
 
+//parsing data in the body of request
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+//innitialize and connect db
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/shopping', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
