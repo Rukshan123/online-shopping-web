ddab6659e7c9d16a7549004bc6d8475f273c7948
Rukshan123 <rukzmadu@gmail.com>
2021-11-12 11:55:33 +0530

    add sample users

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,19 @@
+import bcrypt from 'bcryptjs'
 const data = {
+  users: [
+    {
+      name: 'rukshan',
+      email: 'r@gmail.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: true,
+    },
+    {
+      name: 'maduranga',
+      email: 'm@gmail.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       _id: '1',
