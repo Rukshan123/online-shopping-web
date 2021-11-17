2d96e17b095f7d179558e70e958839a28dfc9127
Rukshan123 <rukzmadu@gmail.com>
2021-11-17 11:46:32 +0530

    create model for save all details about order

---
+++ backend/models/orderModel.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose'
+
+const orderSchema = new mongoose.Schema(
+  {
+    orderItems: [
+      {
+        name: { type: String, required: true },
+        qty: { type: Number, required: true },
+        image: { type: String, required: true },
+        price: { type: Number, required: true },
+        // because the order in this model is link to the product model
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'product',
+          required: true,
+        },
+      },
+    ],
+
+    shippingAddress: {
+      fullName: { type: String, required: true },
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      postalCode: { type: String, required: true },
+      country: { type: String, required: true },
+    },
+
+    paymentMethod: { type: String, required: true },
+    itemsPrice: { type: Number, required: true },
+    shippingPrice: { type: Number, required: true },
+    taxPrice: { type: Number, required: true },
+    totalPrice: { type: Number, required: true },
+    //user that create the order
+    // because the order in this model is link to the user model
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
+    isPaid: { type: Boolean, default: false },
+    paidAt: { type: Date },
+    isDelivered: { type: Boolean, default: false },
+    deliveredAt: { type: Date },
+  },
+  {
+    timestamps: true,
+  }
+)
+const Order = mongoose.model('order', orderSchema)
+export default Order
