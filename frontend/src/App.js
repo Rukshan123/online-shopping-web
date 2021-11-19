445eddac024888164b131071d8fc31a6b831c243
Rukshan123 <rukzmadu@gmail.com>
2021-11-19 12:22:06 +0530

    added orderDetailScreen as new route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Link, Route } from 'react-router-dom'
 import { signOut } from './actions/userActions'
 import CartScreen from './screens/CartScreen'
 import HomeScreen from './screens/HomeScreen'
+import OrderDetailsScreen from './screens/OrderDetailsScreen'
 import PaymentMethodScreen from './screens/PaymentMethodScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import ProductScreen from './screens/ProductScreen'
@@ -64,6 +65,7 @@ function App() {
           <Route path='/shipping' component={ShippingAddressScreen}></Route>
           <Route path='/payment' component={PaymentMethodScreen}></Route>
           <Route path='/placeorder' component={PlaceOrderScreen}></Route>
+          <Route path='/order/:id' component={OrderDetailsScreen}></Route>
           <Route path='/' component={HomeScreen} exact></Route>
         </main>
         <footer className='row center'>All right reserved</footer>
